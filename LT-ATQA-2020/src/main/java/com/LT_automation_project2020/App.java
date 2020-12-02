e4a3933326b1e8f495617d34174b6c5426e53dce
“John <taraslavruk@gmail.com>
2020-12-02 21:02:56 +0200

    created_project_structure

---
+++ LT-ATQA-2020/src/main/java/com/LT_automation_project2020/App.java
@@ -0,0 +1,13 @@
+package com.LT_automation_project2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
